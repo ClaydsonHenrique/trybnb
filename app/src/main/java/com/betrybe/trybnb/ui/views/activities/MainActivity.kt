a26e715a77d91f94c59a790ccc0e941fab2a4175
Claydson Henrique <claydsonhenriquebs@gmail.com>
2024-05-07 17:02:46 -0300

    req 4

--- app/src/main/java/com/betrybe/trybnb/ui/views/activities/MainActivity.kt
+++ app/src/main/java/com/betrybe/trybnb/ui/views/activities/MainActivity.kt
@@ -8,38 +8,43 @@ import com.betrybe.trybnb.ui.views.fragments.CreateReservationFragment
 import com.betrybe.trybnb.ui.views.fragments.ProfileFragment
 import com.betrybe.trybnb.ui.views.fragments.ReservationFragment
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private val bottomNavigation: BottomNavigationView by lazy {
+        findViewById(R.id.navigation_bottom_view)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupBottomNavigationView()
     }
 
-
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.reservation_menu_item -> {
-                loadFragment(R.id.reservation_frame_layout, ReservationFragment())
-                return true
-            }
-            R.id.create_reservation_menu_item -> {
-                loadFragment(R.id.create_reservation_scroll_view, CreateReservationFragment())
-                return true
-            }
-            R.id.profile_menu_tem -> {
-                loadFragment(R.id.profile_scroll_view, ProfileFragment())
-                return true
+    private fun setupBottomNavigationView() {
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.reservation_menu_item -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_fragment_container, ReservationFragment())
+                        .commit()
+                    true
+                }
+                R.id.create_reservation_menu_item -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_fragment_container, CreateReservationFragment())
+                        .commit()
+                    true
+                }
+                R.id.profile_menu_tem -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_fragment_container, ProfileFragment())
+                        .commit()
+                    true
+                }
+                else -> false
             }
         }
-        return super.onOptionsItemSelected(item)
     }
-
-    private fun loadFragment(containerId: Int, fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(containerId, fragment)
-            .commit()
-    }
-
 }
